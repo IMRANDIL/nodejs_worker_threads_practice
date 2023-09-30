ee8c8e3c4365ba76e97d09f9456c4d3e84b4088d
IMRANDIL <aliimranadil2@gmail.com>
2023-09-30 15:55:57 +0530

    added implementation to use the 4 core of the machine

--- four_index.js
+++ four_index.js
@@ -1,37 +1,43 @@
-import express from 'express'
-import {Worker} from 'worker_threads'
+import express from "express";
+import { Worker } from "worker_threads";
 const app = express();
 const THREAD_COUNT = 4;
 
 app.use(express.json());
 
- const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 8000;
 
+const createWorker = () => {
+  const worker = new Worker("./four-worker.js", {
+    workerData: {
+      thread_count: THREAD_COUNT,
+    },
+  });
+  return new Promise((resolve, reject) => {
+    worker.on("message", (data) => {
+      resolve(data);
+    });
 
+    worker.on("error", (error) => {
+      reject(`error: ${error}`);
+    });
+  });
+};
 
- 
+app.get("/non-blocking", (req, res) => {
+  res.status(200).json(`This is non blocking one.`);
+});
 
+app.get("/blocking", async (req, res) => {
+  const promisesArray = [];
+  for (let i = 0; i < THREAD_COUNT; i++) {
+    promisesArray.push(createWorker());
+  }
+  const result = await Promise.all(promisesArray);
+  const resultTosend = result[0] + result[1] + result[2] + result[3];
+  res.status(200).send(`This is blocking one result: ${resultTosend}`);
+});
 
-app.get('/non-blocking', (req,res)=>{
-    res.status(200).json(`This is non blocking one.`)
-})
-
-
-app.get('/blocking', (req,res)=>{
-   const worker = new Worker('./worker.js');
-
-   worker.on('message', (data)=>{
-    res.status(200).send(`This is blocking one result: ${data}`)
-   });
-
-   worker.on('error', (error)=>{
-    res.status(400).send(`error: ${error}`)
-   })
-   
-})
-
-
-
-app.listen(PORT, ()=>{
-    console.log(`server running on port: ${PORT}`)
-})
+app.listen(PORT, () => {
+  console.log(`server running on port: ${PORT}`);
+});
