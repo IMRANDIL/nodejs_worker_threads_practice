aaf773809c79fceb65c2614a87eca217bff7b55a
IMRANDIL <aliimranadil2@gmail.com>
2023-09-30 15:38:05 +0530

    made it asynchronous, offloaded the task from the main thread.

--- index.js
+++ index.js
@@ -1,20 +1,27 @@
 import express from 'express'
+import {Worker} from 'worker_threads'
+const app = express();
 
-const app = express()
+app.use(express.json());
 
-const PORT = process.env.PORT || 6000;
+ const PORT = process.env.PORT || 8000;
 
 app.get('/non-blocking', (req,res)=>{
-    res.status(200).send(`This is non blocking one.`)
+    res.status(200).json(`This is non blocking one.`)
 })
 
 
 app.get('/blocking', (req,res)=>{
-    let counter = 0;
-    for(let i=0; i< 20_000_000_000; i++){
-        counter ++
-    }
-    res.status(200).send(`This is blocking one result: ${counter}`)
+   const worker = new Worker('./worker.js');
+
+   worker.on('message', (data)=>{
+    res.status(200).send(`This is blocking one result: ${data}`)
+   });
+
+   worker.on('error', (error)=>{
+    res.status(400).send(`error: ${error}`)
+   })
+   
 })
 
 
