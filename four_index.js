eefb7f3ab43440d44a6ad4f87048fbf782c4b424
IMRANDIL <aliimranadil2@gmail.com>
2023-09-30 15:49:17 +0530

    adding four workers, using four cpu core of the machine

---
+++ four_index.js
@@ -0,0 +1,37 @@
+import express from 'express'
+import {Worker} from 'worker_threads'
+const app = express();
+const THREAD_COUNT = 4;
+
+app.use(express.json());
+
+ const PORT = process.env.PORT || 8000;
+
+
+
+ 
+
+
+app.get('/non-blocking', (req,res)=>{
+    res.status(200).json(`This is non blocking one.`)
+})
+
+
+app.get('/blocking', (req,res)=>{
+   const worker = new Worker('./worker.js');
+
+   worker.on('message', (data)=>{
+    res.status(200).send(`This is blocking one result: ${data}`)
+   });
+
+   worker.on('error', (error)=>{
+    res.status(400).send(`error: ${error}`)
+   })
+   
+})
+
+
+
+app.listen(PORT, ()=>{
+    console.log(`server running on port: ${PORT}`)
+})
